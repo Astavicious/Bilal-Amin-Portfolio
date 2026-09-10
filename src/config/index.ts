1498d2ef003a5de6fe0de9eb711ed670eb0d05fd
Astavicious <65663903+Astavicious@users.noreply.github.com>
2026-09-10 12:59:05 +0200

    Update LeetCode link in socialLinks

--- src/config/index.ts
+++ src/config/index.ts
@@ -236,7 +236,7 @@ export const SITE_CONFIG: SiteConfig = {
   socialLinks: [
     { text: "LinkedIn", href: "https://www.linkedin.com/in/aminbilal/" },
     { text: "GitHub", href: "https://github.com/Astavicious" },
-    { text: "LeetCode", href: "https://leetcode.com/yourname/" },
+    { text: "LeetCode", href: "https://leetcode.com/u/write2bilalamin/" },
     { text: "Email", href: `mailto:${SITE_CONTENT.hero.email}` },
   ],
   canonicalURL: "https://bilalaminswe.com/",
